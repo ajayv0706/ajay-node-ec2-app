d7348acd2f8db27a538e23fedf38af7bc53f04a6
Ubuntu <ubuntu@ip-172-31-46-14.eu-north-1.compute.internal>
2025-12-09 14:58:50 +0000

    CI/CD deploy test

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Ajay\'s Node.js App on EC2!</h1>');
+  res.send('<h1>Hello from Ajay – CI/CD deploy test!</h1>');
+);
 });
 
 app.listen(PORT, () => {
