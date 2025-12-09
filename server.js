67bb086de86b36217deffbfa117d22be2865b274
Ubuntu <ubuntu@ip-172-31-46-14.eu-north-1.compute.internal>
2025-12-09 15:07:44 +0000

    CI/CD deploy test #2

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const app = express();
 const PORT = 3000;
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello from Ajay – CI/CD deploy test!</h1>');
+  res.send('<h1>Hello from Ajay – CI/CD deploy test #2!</h1>');
 );
 });
 
